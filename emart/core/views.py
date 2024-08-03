5eef80ccede23e4e54ea17574d17540745f974d6
Dhiraj Sarraf <108226247+dhirajs16@users.noreply.github.com>
2024-08-03 04:51:22 +0000

    Restarted the development

---
+++ emart/core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from item.models import Item, Category
+
+# Create your views here.
+def index(request):
+    
+    items = Item.objects.filter(is_sold = False)[:6]
+    categories = Category.objects.all()
+
+    return render(request, 'core/index.html', {
+        # context 
+        'items': items,
+        'categories': categories,
+    })
+
+def contacts(request):
+    return render(request, 'core/contacts.html')
